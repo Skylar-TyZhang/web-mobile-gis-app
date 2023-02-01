6fad905ec06c79375f53d8333825124a6ae11a91
schatty6 <skylarty6@icloud.com>
2023-02-01 19:43:01 +0000

    added my own menu

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,7 @@
 // This is to process the menu functionality
 'user strict'
 let menu1= function(){
+
     
 try {
     alert('This is menu1');
@@ -8,4 +9,14 @@ try {
 } catch (err) {
     alert( err);
 }
+}
+console.log('menu function1')
+
+function popAlert_14(){
+    
+    
+    let re = /([^(]+)@|at ([^(]+) \(/g;
+    let aRegexResult = re.exec(new Error().stack);
+    let sCallerName = aRegexResult[1] || aRegexResult[2];
+    alert("menu number is 12 and menu is called by: "+ sCallerName);
 }
