311fa4a8c862457aca05717500d7bc86ebee5832
schatty6 <skylarty6@icloud.com>
2023-02-03 14:56:53 +0000

    added library files and updated the bootStrap html file

--- js/menu.js
+++ js/menu.js
@@ -12,7 +12,7 @@ try {
 }
 console.log('menu function1')
 
-function popAlert_14(){
+function popAlert_12(){
     
     
     let re = /([^(]+)@|at ([^(]+) \(/g;
@@ -20,3 +20,28 @@ function popAlert_14(){
     let sCallerName = aRegexResult[1] || aRegexResult[2];
     alert("menu number is 12 and menu is called by: "+ sCallerName);
 }
+function popAlert_13(){
+    
+    
+    let re = /([^(]+)@|at ([^(]+) \(/g;
+    let aRegexResult = re.exec(new Error().stack);
+    let sCallerName = aRegexResult[1] || aRegexResult[2];
+    alert("menu number is 13 and menu is called by: "+ sCallerName);
+}
+function popAlert_14(){
+    
+    
+    {let re = /([^(]+)@|at ([^(]+) \(/g;
+    let aRegexResult = re.exec(new Error().stack);
+    let sCallerName = aRegexResult[1] || aRegexResult[2];
+    alert("menu number is 15 and menu is called by: "+ sCallerName);}
+}
+function popAlert_16(){
+    {
+        let re = /([^(]+)@|at ([^(]+) \(/g;
+    let aRegexResult = re.exec(new Error().stack);
+    let sCallerName = aRegexResult[1] || aRegexResult[2];
+    alert("menu number is 16 and menu is called by: "+ sCallerName);
+}
+    console.log(sCallerName)
+};
