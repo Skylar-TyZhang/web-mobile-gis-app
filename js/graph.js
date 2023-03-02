ddd92070b40d1c1268ca093af73dcbd902da7446
schatty6 <skylarty6@icloud.com>
2023-03-02 17:15:48 +0000

    added code to dynamicaly size the graph div and created the svg component to store the graph

--- js/graph.js
+++ js/graph.js
@@ -1,29 +1,38 @@
 'user strict'
 //hide the graph div and show the map div
-function closeAssetData(){
+function closeAssetData() {
     let mapCollapse = document.getElementById('mapWrapper');
     let bsMapCollapse = new bootstrap.Collapse(mapCollapse, {
-    toggle: false, show:false
+        toggle: false, show: false
     });
     bsMapCollapse.show();
     let adwCollapse = document.getElementById('assetDataWrapperWrapper');
     let bsAdwCollapse = new bootstrap.Collapse(adwCollapse, {
-    toggle: false, show:true
+        toggle: false, show: true
     });
     bsAdwCollapse.hide();
-   }
+}
 // show graph menu
-function loadGraph(){
+function loadGraph() {
     let mapCollapse = document.getElementById('mapWrapper');
     let bsMapCollapse = new bootstrap.Collapse(mapCollapse, {
-    toggle: false, show:false
+        toggle: false, show: false
     });
     bsMapCollapse.hide();
     let adwCollapse = document.getElementById('assetDataWrapperWrapper');
     let bsAdwCollapse = new bootstrap.Collapse(adwCollapse, {
-    toggle: false, show:true
+        toggle: false, show: true
     });
     bsAdwCollapse.show();
-   // code to create the graph goes here – see below
-   }
-   
+    // code to create the graph goes here – see below
+    let widtha = document.getElementById("assetDataWrapper").clientWidth * 2;
+    let heighta = document.getElementById("assetDataWrapper").offsetHeight;
+    console.log(widtha + " " + heighta);
+    // Add the close button and an SVG element for the graph
+    document.getElementById("assetDataWrapper").innerHTML = `<div class="h-100 w100">
+ <button type="button" class="btn-close float-end" arialabel="Close" onclick="closeAssetData()"></button>
+ <svg fill="blue" width="`+ widtha + `" height="` + heighta + `" id="svg1">
+ </svg>
+ </div>`
+
+}
