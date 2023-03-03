43017418bd97bdb16ed2574e1849895a65c008b5
schatty6 <skylarty6@icloud.com>
2023-03-03 11:29:22 +0000

    Adapted code in centennial.js so the asset layers can not be added more than once by using array

--- js/centennial.js
+++ js/centennial.js
@@ -2,53 +2,62 @@
 // use AJAX to construct url
 let baseComputerAddress = document.location.origin;
 // use array to track things
-// create an empty array 
-// this needs to be a global variable so that it can be accessed by 
-// all the functions below
-let buildingsLayer = []
+// create an empty array to store asset layers
+let centennialAssets = [];
+let buildingsLayer = [];
+let ethernetcablesLayer = [];
+let roomsLayer = [];
+let sensorsLayer = [];
+
 function loadBuildings() {
     // first check if the thing is loaded already
-    /*
-    for (i = 0; i < buildingsLayer.length; i++) {
-        if (buildingsLayer[i].thingName == building) {
+
+    for (i = 0; i < centennialAssets.length; i++) {
+        if (centennialAssets[i] == 'building') {
             console.log("equal");
-            alert("Thing already loaded");
+            alert("Buildings are already loaded");
             return;
         }
     }
-    */
-    let dataAddress ="/api/geojson/ucfscde/buildings/building_id/location";
+
+    let dataAddress = "/api/geojson/ucfscde/buildings/building_id/location";
     let layerURL = baseComputerAddress + dataAddress;;// construct url as basecomputer address +data address
     $.ajax({
-        url: layerURL, 
-        crossDomain: true, 
+        url: layerURL,
+        crossDomain: true,
         success: function (result) {
             console.log(result); // check that the data is correct
-            buildingsLayer=L.geoJson(result).addTo(mymap)
+            buildingsLayer = L.geoJson(result).addTo(mymap)
             mymap.fitBounds(buildingsLayer.getBounds())
             // now add the thing into the array so that we can reference it later on
             // push adds an item to the top of the array
-            buildingsLayer.push(result);
+            centennialAssets.push('building');
         } // end of the inner function
     }); // end of the ajax request
 }
-function listAllBuildings() {
+function listAllAssets() {
     console.log("*********************************");
     console.log("********Current Things *********");
-    for (i = 0; i < buildingsLayer.length; i++) {
-        console.log(buildingsLayer[i].thingName);
+    for (i = 0; i < centennialAssets.length; i++) {
+        console.log(centennialAssets[i]);// get names of assets that are already loaded
     }
     console.log("*********************************");
 }
-function removeBuilding(building) {
-    for (i = 0; i < buildingsLayer.length; i++) {
-        if (buildingsLayer[i].thingname == building) {
-            console.log("equal");
-            buildingsLayer.splice(i, 1);
+function removeBuildings() {
+    for (i = 0; i < centennialAssets.length; i++) {
+        if (centennialAssets[i] == 'building') {
+            console.log("equal asset type");
+            centennialAssets.splice(i, 1);
 
             // don't continue the loop as we now have 1 less element in the array which means 
             // that when we try to get the last element it won't be there any more
             break;
         }
+    }
+    try {
+        alert('the building data will be removed');
+        mymap.removeLayer(buildingsLayer);
+    } catch (err) {
+        alert('Layer does not exist:' + err);
     }
 }
