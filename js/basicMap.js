97ffa86f14894dd9526b3783270b913dee595e62
schatty6 <skylarty6@icloud.com>
2023-03-16 18:32:59 +0000

    Added function setUpPointClick to create a point and set up the onclick behaviour for the point.

--- js/basicMap.js
+++ js/basicMap.js
@@ -33,84 +33,58 @@ let width; // NB – keep this as a global variable
 //let popup; // keep this as a global variable
 let mapPoint; // store the geoJSON feature so that we can remove it if the screen is resized
 function setMapClickEvent() {
- // get the window width
- width = $(window).width();
-// we use the bootstrap Medium and Large options for the asset location capture
-// and the small and XS options for the condition option
-// see here: https://www.w3schools.com/bootstrap/bootstrap_grid_system.asp
- if (width < 992) { 
-//the condition capture –
-//anything smaller than 992px is defined as 'medium' by bootstrap
- // remove the map point if it exists
- if (mapPoint){
- mymap.removeLayer(mapPoint);
- }
- // cancel the map onclick event using off ..
- mymap.off('click',onMapClick)
- // set up a point with click functionality
- // so that anyone clicking will add asset condition information
- setUpPointClick();
-}
- else { // the asset creation page
- // remove the map point if it exists
- if (mapPoint){
- mymap.removeLayer(mapPoint);
- }
- // the onclik functionality of MAP pops up a blank asset creation form
- mymap.on('click',onMapClick);
-}
-}
+    // get the window width
+    width = $(window).width();
+    // we use the bootstrap Medium and Large options for the asset location capture
+    // and the small and XS options for the condition option
+    // see here: https://www.w3schools.com/bootstrap/bootstrap_grid_system.asp
+    if (width < 992) {
+        //the condition capture –
+        //anything smaller than 992px is defined as 'medium' by bootstrap
+        // remove the map point if it exists
+        if (mapPoint) {
+            mymap.removeLayer(mapPoint);
+        }
+        // cancel the map onclick event using off ..
+        mymap.off('click', onMapClick)
+        // set up a point with click functionality
+        // so that anyone clicking will add asset condition information
+        setUpPointClick();
+    }
+    else { // the asset creation page
+        // remove the map point if it exists
+        if (mapPoint) {
+            mymap.removeLayer(mapPoint);
+        }
+        // the onclik functionality of MAP pops up a blank asset creation form
+        mymap.on('click', onMapClick);
+    }
+};
 
-/*let testMarkerPink = L.AwesomeMarkers.icon({
-    icon: 'play',
-    markerColor: 'pink'
-});
-let testMarkerGreen = L.AwesomeMarkers.icon({
-    icon: 'play',
-    markerColor: 'green'
-});
-function addBasicMarkers() {
-    // add circle 
-    console.log('addBasicMarker function')
-    L.circle([51.508, -0.11], 5000, {
-        color: 'green',
-        fillColor: '#f03',
-        fillOpacity: 0.8
-    }).addTo(mymap).bindPopup('This is a circle.');
 
-    console.log('added a circle');
-
-    // add a polygon
-    let myPolygon = L.polygon([
-        [51.709, -0.10],
-        [51.703, 0.07],
-        [51.22, 0.07],
-        [51.22, -0.057]
-    ], {
-        color: 'orange',
-        fillColor: '#f03',
-        fillOpacity: 0.5
-    }).addTo(mymap).bindPopup('This is a polygon in 2023.');
-    console.log('data item created');
-    // create a geoJSON feature -
+//Create a point and set up the onlick behaviour for the point
+function setUpPointClick() {
+    // create a geoJSON feature (in your assignment code this will be replaced
+    // by an AJAX call to load the asset points on the map
     let geojsonFeature = {
         "type": "Feature",
         "properties": {
             "name": "London",
-            "popupContent": "This is where UCL is based. We have on campus and off campus activity."
+            "popupContent": "This is where UCL is based"
         },
         "geometry": {
             "type": "Point",
-            "coordinates": [-0.133583, 51.524776]
+            "coordinates": [-0.13263, 51.522449]
         }
     };
-    // add it to the map
-    L.geoJSON(geojsonFeature, {
-        pointToLayer: function (feature, latlng) {
-            return L.marker(latlng, { icon: testMarkerPink });
-        }
-    }).addTo(mymap).bindPopup('<b>' + geojsonFeature.properties.name + '' + geojsonFeature.properties.popupContent + '<b>');
+    // and add it to the map and zoom to that location
+    // use the mapPoint variable so that we can remove this point layer on
+    mapPoint = L.geoJSON(geojsonFeature).addTo(mymap).bindPopup(popUpHTML);
+    mymap.setView([51.522449, -0.13263], 12)
+    // the on click functionality of the POINT should pop up partially populated condition form so that 
+    //the user can select the condition they require
+    let popUpHTML = getPopupHTML;
+    console.log(popUpHTML);
+}
 
 
-}// end code to add the basic markers
-*/
