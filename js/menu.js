fd6e74e49436bd89eebd6ab10455251664be2d0d
schatty6 <skylarty6@icloud.com>
2023-03-27 18:55:33 +0100

    Added 'dailyReportingRatesGraph()' funtion in the menu.js for the menu to load daily reporting rates graph (for now the function will only alert user the name of function.

--- js/menu.js
+++ js/menu.js
@@ -10,3 +10,13 @@ function assetInBestCondition(){
 }
     console.log(sCallerName)
 };
+//
+function dailyReportingRatesGraph(){
+    {
+        let re = /([^(]+)@|at ([^(]+) \(/g;
+    let aRegexResult = re.exec(new Error().stack);
+    let sCallerName = aRegexResult[1] || aRegexResult[2];
+    alert("This menu is called by: "+ sCallerName);
+}
+    console.log(sCallerName)
+};
