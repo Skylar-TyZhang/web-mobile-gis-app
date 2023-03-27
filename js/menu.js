8f484caaf35d148cf9a9b911f37985770a0e7226
schatty6 <skylarty6@icloud.com>
2023-03-27 18:57:38 +0100

    Added 'menu()' funtion in the menu.js for the menu to load documentation when clicked (for now the function will only alert user the name of function.

--- js/menu.js
+++ js/menu.js
@@ -10,8 +10,19 @@ function assetInBestCondition(){
 }
     console.log(sCallerName)
 };
-//
+//load reporting rate graph
 function dailyReportingRatesGraph(){
+    {
+        let re = /([^(]+)@|at ([^(]+) \(/g;
+    let aRegexResult = re.exec(new Error().stack);
+    let sCallerName = aRegexResult[1] || aRegexResult[2];
+    alert("This menu is called by: "+ sCallerName);
+}
+    console.log(sCallerName)
+};
+// load help documentation
+
+function help(){
     {
         let re = /([^(]+)@|at ([^(]+) \(/g;
     let aRegexResult = re.exec(new Error().stack);
