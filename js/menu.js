aa23f0529c7a9713b591bef22115b6cd8e0bf89a
schatty6 <skylarty6@icloud.com>
2023-03-27 19:00:14 +0100

    Added 'getUserRanking()' funtion in the menu.js for the menu to load user ranking when clicked (for now the function will only alert user the name of function.

--- js/menu.js
+++ js/menu.js
@@ -31,3 +31,13 @@ function help(){
 }
     console.log(sCallerName)
 };
+// get user ranking
+function getUserRanking(){
+    {
+        let re = /([^(]+)@|at ([^(]+) \(/g;
+    let aRegexResult = re.exec(new Error().stack);
+    let sCallerName = aRegexResult[1] || aRegexResult[2];
+    alert("This menu is called by: "+ sCallerName);
+}
+    console.log(sCallerName)
+};
