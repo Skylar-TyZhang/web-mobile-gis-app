e2fb46a5f8cfa1a039a5d41c0f2318858c3c3bf7
schatty6 <skylarty6@icloud.com>
2023-04-13 18:08:53 +0100

    Adapted code of function checkCondition(id), now the checked condition description will be recorded as provided by the database instead of hardcoded. The asset_id was also passed to the function to make sure the condition description is provided for specified asset.

--- js/uploadData.js
+++ js/uploadData.js
@@ -25,47 +25,32 @@ async function saveNewAsset() {
 }
 
 
-function checkCondition(id) {
-    console.log('Get information in condition report')
-
-    let assetID = document.getElementById(`conditionForm_${id}`).innerHTML;
-    console.log(assetID)   // use console to check the div id 
-    let assetName = document.getElementById('asset_name').innerHTML;
+async function checkCondition(id) {
+    let conditions = await getconditionDetails();
+    console.log('check condition')
+    let asset_name = document.getElementById('asset_name').innerHTML;
     let assetInstallationDate = document.getElementById('installation_date').innerHTML;
+    
     let user_id = document.getElementById('user_id').innerHTML;
-
+    
     let postString = //'assetID='+assetID+
-        "&assetName=" + assetName +
+        "&asset_name=" + asset_name +
         "&assetInstallationDate=" + assetInstallationDate +
         '&user_id=' + user_id;
-    // get information from a radio    
-    if (document.getElementById(`condition1_${id}`).checked) {
-        postString = postString + "&condition=As new or in good serviceable condition";
-    };
-
-    if (document.getElementById(`condition2_${id}`).checked) {
-        postString = postString + "&condition=Deteriorating, evidence of high usage, age, additional maintenance costs and inefficiency ";
-    };
-    if (document.getElementById(`condition3_${id}`).checked) {
-        postString = postString + "&condition=Requires replacement within 5 years ";
-    };
-
-    if (document.getElementById(`condition4_${id}`).checked) {
-        postString = postString + "&condition=In poor condition, overdue for replacement ";
-    };
-
-    if (document.getElementById(`condition5_${id}`).checked) {
-        postString = postString + "&condition=Unable to determine condition (e.g. as item is hidden)";
-    };
-
-    if (document.getElementById(`condition6_${id}`).checked) {
-        postString = postString + "&condition=Item does not exist";
-    };
-
+       // console.log(postString)
+    // get information from a radio  
+    for  (let i=0;i<conditions.length; i++){        
+        if(document.getElementById(`condition_${i+1}`).checked){
+            
+            let condition_description=conditions[i]["condition_description"]
+            
+            postString+="&condition_description="+condition_description;
+        }
+    }
     let previousCondition = document.getElementById(`previousCondition_${id}`).innerHTML;
     postString = postString + '&previousCondition=' + previousCondition;
-    console.log('condition:' + postString)
-    processData(postString);
+    console.log(postString)
+    //processData(postString);
 
 }
 function processData(postString) {
