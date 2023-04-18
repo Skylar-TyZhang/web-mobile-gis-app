095c9d5cb0b6cfe3353663b381cefb4c0bdc4938
schatty6 <skylarty6@icloud.com>
2023-04-18 11:59:33 +0100

    Changed the menu function addLast5Reports() by retriving the geoJSON data of last 5 reports created by specific user. So far, the layer of 5 last reports will be added to the map if the user click on the menu 'Add Layer -last 5 reports color coded'.

--- js/menu.js
+++ js/menu.js
@@ -68,14 +68,13 @@ function remove5ClosestAssets() {
     console.log(sCallerName)
 };
 // Add Layer -last 5 reports,color coded 
-function addLast5Reports() {
-    {
-        let re = /([^(]+)@|at ([^(]+) \(/g;
-        let aRegexResult = re.exec(new Error().stack);
-        let sCallerName = aRegexResult[1] || aRegexResult[2];
-        alert("This menu is called by: " + sCallerName);
-    }
-    console.log(sCallerName)
+let last5ReportsLayer=[];
+async function addLast5Reports() {
+    let user_id=await getUserId();
+    let res = await getData(`/api/geojson/lastFiveConditionReports/${user_id}`);
+    console.log(res);
+    lastReportLayer=L.geoJSON(res).addTo(mymap);
+
 };
 // Remove Layer -last 5 reports,color coded 
 function removeLast5Reports() {
