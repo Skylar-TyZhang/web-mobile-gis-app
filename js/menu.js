51f3cc624400233157f25c24c6412279e32e88ee
schatty6 <skylarty6@icloud.com>
2023-04-18 14:42:27 +0100

    Added condition codes in function addLast5Reports() so when there is no data of last 5 reports retrived the user will be notified.

--- js/menu.js
+++ js/menu.js
@@ -76,104 +76,110 @@ async function addLast5Reports() {
     let conditions = await getconditionDetails();
     let res = await getData(`/api/geojson/lastFiveConditionReports/${user_id}`);
     console.log(res);
-    last5ReportsLayer = L.geoJSON(res, {
-        onEachFeature(feature) {
-            console.log(feature.properties)
-        },
-        pointToLayer: function (feature, latlng) {
-            let featureCondition = feature.properties['condition_description'];
-
-            let conditionMarker0 = L.AwesomeMarkers.icon({
-                icon: 'play',
-                markerColor: 'blue',
-            });
-            let conditionMarker1 = L.AwesomeMarkers.icon({
-                icon: 'play',
-                markerColor: 'green',
-            });
-            let conditionMarker2 = L.AwesomeMarkers.icon({
-                icon: 'play',
-                markerColor: 'pink',
-            });
-            let conditionMarker3 = L.AwesomeMarkers.icon({
-                icon: 'play',
-                markerColor: 'purple',
-            });
-            let conditionMarker4 = L.AwesomeMarkers.icon({
-                icon: 'play',
-                markerColor: 'orange',
-            });
-            let conditionMarker5 = L.AwesomeMarkers.icon({
-                icon: 'play',
-                markerColor: 'gray',
-            });
-
-
-            if (featureCondition == conditions[0]['condition_description']) {
-
-                return L.marker(latlng, { icon: conditionMarker0 })
-            }
-            else if (featureCondition == conditions[1]['condition_description']) {
-
-                return L.marker(latlng, { icon: conditionMarker1 })
-            }
-            else if (featureCondition == conditions[2]['condition_description']) {
-                return L.marker(latlng, { icon: conditionMarker2 })
-            }
-            else if (featureCondition == conditions[3]['condition_description']) {
-                return L.marker(latlng, { icon: conditionMarker3 })
-            }
-            else if (featureCondition == conditions[4]['condition_description']) {
-                return L.marker(latlng, { icon: conditionMarker4 })
-            }
-            else if (featureCondition == conditions[5]['condition_description']) {
-                return L.marker(latlng, { icon: conditionMarker5 })
-            }
-        },
-
-    }).addTo(mymap);
-
-
-};
-// Remove Layer -last 5 reports,color coded 
-function removeLast5Reports() {
-    try {
-        alert('The assets with last report information will be removed.');
-        mymap.removeLayer(last5ReportsLayer);
-        setUpPointClick();
-    } catch (err) {
-        alert("Sorry, you haven't loaded the assets with last report informatio so there is nothing to remove.");
-        console.log(err)
-    }
-};
-// Add Layer - not rated in the last 3 days
-let notRatedLayer = [];   // layer to store asset that has not been assessed in the past 3 days
-async function addNotRated() {
-    let user_id = await getUserId();
-    let res = await getData(`/api/geojson/conditionReportMissing/${user_id}`);
-    //console.log(res);
     console.log(res[0].features);
     if (res[0].features != null) {
-        console.log('The assets that were not rated in the past 3 days exist.')
+        console.log('Last 5 reports exist.')
+        last5ReportsLayer = L.geoJSON(res, {
+            onEachFeature(feature) {
+                console.log(feature.properties)
+            },
+            pointToLayer: function (feature, latlng) {
+                let featureCondition = feature.properties['condition_description'];
 
-        notRatedLayer = L.geoJSON(res).addTo(mymap);
-        // zoom to the asset points
-        mymap.fitBounds(notRatedLayer.getBounds());
+                let conditionMarker0 = L.AwesomeMarkers.icon({
+                    icon: 'play',
+                    markerColor: 'blue',
+                });
+                let conditionMarker1 = L.AwesomeMarkers.icon({
+                    icon: 'play',
+                    markerColor: 'green',
+                });
+                let conditionMarker2 = L.AwesomeMarkers.icon({
+                    icon: 'play',
+                    markerColor: 'pink',
+                });
+                let conditionMarker3 = L.AwesomeMarkers.icon({
+                    icon: 'play',
+                    markerColor: 'purple',
+                });
+                let conditionMarker4 = L.AwesomeMarkers.icon({
+                    icon: 'play',
+                    markerColor: 'orange',
+                });
+                let conditionMarker5 = L.AwesomeMarkers.icon({
+                    icon: 'play',
+                    markerColor: 'gray',
+                });
+
+
+                if (featureCondition == conditions[0]['condition_description']) {
+
+                    return L.marker(latlng, { icon: conditionMarker0 })
+                }
+                else if (featureCondition == conditions[1]['condition_description']) {
+
+                    return L.marker(latlng, { icon: conditionMarker1 })
+                }
+                else if (featureCondition == conditions[2]['condition_description']) {
+                    return L.marker(latlng, { icon: conditionMarker2 })
+                }
+                else if (featureCondition == conditions[3]['condition_description']) {
+                    return L.marker(latlng, { icon: conditionMarker3 })
+                }
+                else if (featureCondition == conditions[4]['condition_description']) {
+                    return L.marker(latlng, { icon: conditionMarker4 })
+                }
+                else if (featureCondition == conditions[5]['condition_description']) {
+                    return L.marker(latlng, { icon: conditionMarker5 })
+                }
+            },
+
+        }).addTo(mymap);
     }
-    else {
-        console.log('No data retrived');
-        alert('There is nothing to show.\n All of your assets have been rated in the last 3 days. Good job!')
+    else{console.log('No data retrived');
+    alert('Sorry, we cannot retrive the last 5 reports for you.')
+}
+
     };
+    // Remove Layer -last 5 reports,color coded 
+    function removeLast5Reports() {
+        try {
+            alert('The assets with last report information will be removed.');
+            mymap.removeLayer(last5ReportsLayer);
+            setUpPointClick();
+        } catch (err) {
+            alert("Sorry, you haven't loaded the assets with last report informatio so there is nothing to remove.");
+            console.log(err)
+        }
+    };
+    // Add Layer - not rated in the last 3 days
+    let notRatedLayer = [];   // layer to store asset that has not been assessed in the past 3 days
+    async function addNotRated() {
+        let user_id = await getUserId();
+        let res = await getData(`/api/geojson/conditionReportMissing/${user_id}`);
+        //console.log(res);
+        console.log(res[0].features);
+        if (res[0].features != null) {
+            console.log('The assets that were not rated in the past 3 days exist.')
 
-};
-// Remove Layer - not rated in the last 3 days
-function removeNotRated() {
-    try {
-        //alert('The assets with last report information will be removed.');
-        mymap.removeLayer(notRatedLayer);
-        setUpPointClick();
-    } catch (err) {
-        alert("Sorry, you haven't loaded the assets with last report informatio so there is nothing to remove.");
-        console.log(err)
-    }
-};
+            notRatedLayer = L.geoJSON(res).addTo(mymap);
+            // zoom to the asset points
+            mymap.fitBounds(notRatedLayer.getBounds());
+        }
+        else {
+            console.log('No data retrived');
+            alert('There is nothing to show.\n All of your assets have been rated in the last 3 days. Good job!')
+        };
+
+    };
+    // Remove Layer - not rated in the last 3 days
+    function removeNotRated() {
+        try {
+            //alert('The assets with last report information will be removed.');
+            mymap.removeLayer(notRatedLayer);
+            setUpPointClick();
+        } catch (err) {
+            alert("Sorry, you haven't loaded the assets with last report informatio so there is nothing to remove.");
+            console.log(err)
+        }
+    };
