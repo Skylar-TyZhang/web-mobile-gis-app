1ca5fdb180e06c9f45a040d49ab458f749c3b936
schatty6 <skylarty6@icloud.com>
2023-04-22 16:41:51 +0100

    Created a bar graph showing asset data name and condition description value using chart.js. The data was retrieved from API to show the graph on the dashboard html.

--- assets/js/charts-bars.js
+++ assets/js/charts-bars.js
@@ -1,25 +1,19 @@
-/**
+/*
  * For usage, visit Chart.js docs https://www.chartjs.org/docs/latest/
  */
 const barConfig = {
   type: 'bar',
   data: {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    labels: xlabel,
     datasets: [
       {
         label: 'Shoes',
         backgroundColor: '#0694a2',
         // borderColor: window.chartColors.red,
         borderWidth: 1,
-        data: [-3, 14, 52, 74, 33, 90, 70],
+        data: conditionData,
       },
-      {
-        label: 'Bags',
-        backgroundColor: '#7e3af2',
-        // borderColor: window.chartColors.blue,
-        borderWidth: 1,
-        data: [66, 33, 43, 12, 54, 62, 84],
-      },
+      
     ],
   },
   options: {
@@ -32,4 +26,3 @@ const barConfig = {
 
 const barsCtx = document.getElementById('bars')
 window.myBar = new Chart(barsCtx, barConfig)
-let dataURL= baseComputerAddress + '/api/geojson/dailyParticipationRates';
